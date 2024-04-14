f26b6f1b9b20f1730e2f0cf5a5c298702223df6d
Abuzar Zafar <sume62800@gmail.com>
2024-04-14 15:28:28 +0530

    add redux tool kit to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { conf } from './conf/conf'
+import authService from "./appwrite/auth"
+import { login, logout } from './store/authSlice'
+import { useDispatch } from 'react-redux'
+import Header from './compoenents/hearder/header'
+import Footer from './compoenents/footer/footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const dispatch=useDispatch();
   
-  console.log(conf.appwrite_url)
-  return (
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if (userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+  return !loading ? (
     <>
       <div>
-       <h1>working with react app</h1>
-       
+        <Header/>
+        <Footer/>
       </div>
     </>
-  )
+  ): null
 }
 
 export default App
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -3,7 +3,7 @@ const conf={
     appwrite_url:import.meta.env.VITE_APPWRITE_URL,
     database_id:import.meta.env.VITE_APPWRITE_DATABASE_ID,
     collection_id:import.meta.env.VITE_APPWRITE_COLLECTION_ID,
-    bucket_id:import.meta.env.VITE_APPWRITE_BUCKET_ID
+    bucket_id :import.meta.env.VITE_APPWRITE_BUCKET_ID
 }
 
 
